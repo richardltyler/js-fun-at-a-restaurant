f7ba499b7c41ca067af6afd34a8edc277a045a61
Richard Tyler <richardtyler2011@gmail.com>
2020-10-11 21:02:10 -0600

    Complete second function, addMenuItem

--- src/restaurant.js
+++ src/restaurant.js
@@ -9,10 +9,24 @@ function createRestaurant (restaurantName) {
     };
     return restaurant;
 
-};
+}
+
+function addMenuItem (restaurantName, menuItem) {
+  if(menuItem.type == 'lunch' && restaurantName.menus.lunch.includes(menuItem) == false) {
+      restaurantName.menus.lunch.push(menuItem);
+  } else if(menuItem.type == 'breakfast' && restaurantName.menus.breakfast.includes(menuItem) == false) {
+    restaurantName.menus.breakfast.push(menuItem);
+  } else if(menuItem.type == 'dinner' && restaurantName.menus.dinner.includes(menuItem) == false){
+    restaurantName.menus.dinner.push(menuItem);
+  };
+}
+
+function removeMenuItem () {
+
+}
 
 module.exports = {
   createRestaurant,
-  // addMenuItem,
-  // removeMenuItem
+  addMenuItem,
+  removeMenuItem
 }
